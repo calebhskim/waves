a1c6104fb23e749451487c62a47e1877a6df699b
Lukas Froehlich <lukasfroehlich1@gmail.com>
2016-04-30 12:33:09 -0700

    added spotify node

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var SpotifyWebApi = require('spotify-web-api-node');
 var app = express();
 
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : 'fcecfc72172e4cd267473117a17cbd4d',
+  clientSecret : 'a6338157c9bb5ac9c71924cb2940e1a7',
+  redirectUri : 'http://www.example.com/callback'
+});
 
 app.get('/', function (req, res) {
       res.send('Hello World!');
