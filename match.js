0ff7c0bc737156e2ee297767095cb1cc5628da22
Lukas Froehlich <lukasfroehlich1@gmail.com>
2016-05-01 04:33:36 -0700

    added genressss

--- match.js
+++ match.js
@@ -7,8 +7,24 @@ l_artists = jsonfile.readFileSync('lukas_artists.json');
 c_tracks = jsonfile.readFileSync('caleb_tracks.json');
 c_artists = jsonfile.readFileSync('caleb_artists.json');
 
+freq_count = function(arr) {
+    hash = {};
+
+    for (var i in arr) {
+        if (arr[i] in hash) {
+            hash[arr[i]] += 1;
+        }
+        else {
+            hash[arr[i]] = 1;
+        }
+    }
+
+    return hash;
+};
+    
+
 match.match = function(p1_track, p1_artist, p2_track, p2_artist, callback) {
-	//setting up ID arrays 
+    //setting up ID arrays 
     var p1_track_id = [], p1_artist_id = [], p2_track_id = [], p2_artist_id = [];
 
     p1_track_id = p1_track.map(function(x) { return x.id; });
@@ -22,38 +38,38 @@ match.match = function(p1_track, p1_artist, p2_track, p2_artist, callback) {
 
     for (i = 0; i < p1_track.length; i++){
 
-    	var p2t_index = p2_track_id.indexOf(p1_track_id[i]);
-    	var rank1 = Math.floor((i / 10));
+        var p2t_index = p2_track_id.indexOf(p1_track_id[i]);
+        var rank1 = Math.floor((i / 10));
 
-    	if (p2t_index > -1){
-    		common_tracks.push(p1_track[i].id);
-    		var rank2 = Math.floor((p2t_index / 10));
-    		if (rank2 == rank1){
-    			tp += (Math.floor((p1_track.length / 10)) - rank2);
-    			total_tp += (Math.floor((p1_track.length / 10)) - rank2);
-    		} else if (rank2 < rank1){
-    			total_tp += (Math.floor((p1_track.length / 10)) - rank2);
-    		} else {
-    			total_tp += (Math.floor((p1_track.length / 10)) - rank1);
-    		}
-    	}
+        if (p2t_index > -1){
+            common_tracks.push(p1_track[i].id);
+            var rank2 = Math.floor((p2t_index / 10));
+            if (rank2 == rank1){
+                tp += (Math.floor((p1_track.length / 10)) - rank2);
+                total_tp += (Math.floor((p1_track.length / 10)) - rank2);
+            } else if (rank2 < rank1){
+                total_tp += (Math.floor((p1_track.length / 10)) - rank2);
+            } else {
+                total_tp += (Math.floor((p1_track.length / 10)) - rank1);
+            }
+        }
     }
-    
-    for (i = 0; i < p1_artist.length; i++){
-    	var p2a_index = p2_artist_id.indexOf(p1_artist_id[i]);
 
-    	if (p2a_index > -1){
-    		common_artists.push(p1_artist[i].id);
-    		rank2 = Math.floor((p2a_index / 10));
-    		if (rank2 == rank1){
-    			ap += (Math.floor((p1_artist.length / 10)) - rank2);
-    			total_ap += (Math.floor((p1_artist.length / 10)) - rank2);
-    		} else if (rank2 < rank1){
-    			total_ap += (Math.floor((p1_artist.length / 10)) - rank2);
-    		} else {
-    			total_ap += (Math.floor((p1_artist.length / 10)) - rank1);
-    		}
-    	}
+    for (i = 0; i < p1_artist.length; i++){
+        var p2a_index = p2_artist_id.indexOf(p1_artist_id[i]);
+
+        if (p2a_index > -1){
+            common_artists.push(p1_artist[i].id);
+            rank2 = Math.floor((p2a_index / 10));
+            if (rank2 == rank1){
+                ap += (Math.floor((p1_artist.length / 10)) - rank2);
+                total_ap += (Math.floor((p1_artist.length / 10)) - rank2);
+            } else if (rank2 < rank1){
+                total_ap += (Math.floor((p1_artist.length / 10)) - rank2);
+            } else {
+                total_ap += (Math.floor((p1_artist.length / 10)) - rank1);
+            }
+        }
     }
 
 
@@ -61,39 +77,57 @@ match.match = function(p1_track, p1_artist, p2_track, p2_artist, callback) {
     var track_points = 0, artist_points = 0;
 
     if (common_tracks.length != 0){
-    	if ((tp == 0) && (total_tp == 0)){
-    		total_tp = 1;
-    		tp = 1;
-    	}
-    	track_points = (tp/total_tp);
+        if ((tp == 0) && (total_tp == 0)){
+            total_tp = 1;
+            tp = 1;
+        }
+        track_points = (tp/total_tp);
     }
 
     if (common_artists.length != 0){
-    	if ((ap == 0) && (total_ap == 0)){
-    		total_ap = 1;
-    		ap = 1;
-    	}
-    	artist_points = (ap/total_ap);
+        if ((ap == 0) && (total_ap == 0)){
+            total_ap = 1;
+            ap = 1;
+        }
+        artist_points = (ap/total_ap);
     }
 
-    spotify_connect.related_artists(p1_artist, p2_artist, function(err, res) {
-		intersect_length = Math.max(Math.min(res[0].length, res[1].length), 1);
-		intersect = res[0].filter(function(n) {
-			return res[1].indexOf(n) != -1;
-		});
+    var p1_genres = [].concat.apply([], p1_artist.map(function(x) { return x.genres; }));
+    var p2_genres = [].concat.apply([], p2_artist.map(function(x) { return x.genres; }));
 
-        var compatibility = (common_tracks.length/p1_track.length)*(0.35) + track_points*(0.05) + 
-                            (common_artists.length/p1_artist.length)*(0.4375) + artist_points*(0.0625) +
-							(intersect.length / intersect_length)*(0.1);
-        
+    p1_hash = freq_count(p1_genres);
+    p2_hash = freq_count(p2_genres);
+
+    genre_under = Math.min(p1_genres.length, p2_genres.length);
+
+    key = Object.keys(p1_hash);
+    genre_overall = 0;
+
+    for (var i = 0; i < key.length; i++) {
+        if (key[i] in p1_hash && key[i] in p2_hash) {
+            genre_overall += Math.min(p1_hash[key[i]], p2_hash[key[i]]);
+        }
+    } 
+
+    spotify_connect.related_artists(p1_artist, p2_artist, function(err, res) {
+        related_intersect_length = Math.max(Math.min(res[0].length, res[1].length), 1);
+        related_intersect = res[0].filter(function(n) {
+            return res[1].indexOf(n) != -1;
+        });
+
+        var compatibility = (common_tracks.length/p1_track.length)*(0.25) + track_points*(0.05) + 
+            (common_artists.length/p1_artist.length)*(0.25) + artist_points*(0.05) +
+            (related_intersect.length / related_intersect_length)*(0.07) + 
+            (genre_overall / genre_under)*(0.33);
+
+        console.log(common_artists);
+
+        console.log(compatibility);
         callback(null, compatibility); 
         /**should pass in common tracks and artists for display*/ 
         // you put the percentage in here when youre done
     });
-
-
-
-}
+};
 
 
 // testing!
@@ -112,6 +146,6 @@ l_artists = jsonfile.readFileSync('lukas_artists.json');
 c_tracks = jsonfile.readFileSync('caleb_tracks.json');
 c_artists = jsonfile.readFileSync('caleb_artists.json');
 
-//match.match(l_tracks, l_artists, c_tracks, c_artists, function(res) {
-//    console.log(res);
-//});
+match.match(l_tracks, l_artists, c_tracks, c_artists, function(res) {
+    console.log(res);
+});
